c9ea7496bbb00bbf630449b7876be58beda32968
Marco S <70583350+MarcoSa-2000@users.noreply.github.com>
2022-05-19 18:46:48 +0200

    +

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='anime-gifs.py',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Get random anime gifs by category.',
+    author='Marco-Sa2000',
+    license='MIT',
+    url="",
+    python_requires='>=3.8',
+)
---
+++ utils/gifs.py
@@ -0,0 +1,1 @@
+attack = ['https://cdn.discordapp.com/attachments/921455758330200135/921455815787958302/FdXPLmh.gif']
